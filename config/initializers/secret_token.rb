be129dc9ae8fbe593d1a69bd6b0ea3cad0a3dbc7
' <'>
2014-01-23 12:41:31 -0600

    Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twiapp::Application.config.secret_key_base = '3d68f72811fad5a99acf3558a5d2e47147cf68f5e43080088fbbd1e0d725a60b384f66e3aa22393fe910f428d3a2c20ef76f3174af64e48afcdd2bebda376e05'
